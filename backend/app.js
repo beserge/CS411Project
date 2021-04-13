dadb16df2397bc7907bf70aaa270a4fb1462a485
U-MSI\Ben <bensergentanis@gmail.com>
2021-04-13 13:45:32 -0400

    working with frontend

--- backend/app.js
+++ backend/app.js
@@ -18,33 +18,25 @@ const RegSchema = new Schema({
         gender: String,
         goalLbs: Number,
         timeDays: Number,
-        dietMacros: {
-            carbs: Number,
-            fats: Number,
-            protein: Number,
-        },
-        dietRestrictions: {
-            isKosher: Boolean,
-            isHalal: Boolean,
-            isVegetarian: Boolean,
-            isVegan: Boolean,
-        },
-        workoutDays: {
-            sunday: Boolean,
-            monday: Boolean,
-            tuesday: Boolean,
-            wednesday: Boolean,
-            thursday: Boolean,
-            friday: Boolean,
-            saturday: Boolean,
-        },
-        workoutTimes: Array, //preferred times in min since midnight, so 10:30 AM = 10*60 + 30
-        workoutTypes: {
-            indoor: Boolean,
-            outdoor: Boolean,
-            cycling: Boolean,
-            running: Boolean,
-        },
+		carbs: Number,
+		fats: Number,
+		protein: Number,
+		isKosher: Boolean,
+		isHalal: Boolean,
+		isVegetarian: Boolean,
+		isVegan: Boolean,	
+		sunday: Boolean,
+		monday: Boolean,
+		tuesday: Boolean,
+		wednesday: Boolean,
+		thursday: Boolean,
+		friday: Boolean,
+		saturday: Boolean,
+        workoutTimes: Number, //preferred time in min since midnight
+		indoor: Boolean,
+		outdoor: Boolean,
+		cycling: Boolean,
+		running: Boolean,
 });
 
 let RegData = mongoose.model('RegData', RegSchema)
@@ -59,9 +51,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function RegHandler (req, res){
-
-    //RegData.deleteMany(function(err){})
-
     console.log(req.query)
     let regdata = new RegData(req.query);
     regdata.save(function (err){ if (err) console.log(err) })
@@ -69,10 +58,21 @@ function RegHandler (req, res){
     RegData.find(function(err, items){
         if(err) return console.error(err)
         let reply = JSON.stringify(items)
-        res.send(reply);
+        console.log(reply);
     })
 }
 
+
+//deletes the database
+//rm this soon, just for debugging
+function DeleteHandler(req,res){
+	console.log("delete whole db!!!")
+    RegData.deleteMany(function(err){if(err) console.log(err) })
+
+	res.send("complete!")
+}
+
 app.get("/reg", RegHandler)
+app.get("/del_all", DeleteHandler)
 
 module.exports = app;
