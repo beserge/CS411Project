2145475f6a35d3ba9108211df48ce73b81d6c717
Tegveer <tegveerg>
2021-04-17 14:49:45 -0400

    splashscreen second commit

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SplashScreenComponent } from './splashscreen/splashscreen.component';
 import { RegComponent } from './reg/reg.component';
 const routes: Routes = [];
 @NgModule({
