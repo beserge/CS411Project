66dece8a62b73f73cee87716628c7d76d7290c15
beserge <bensergentanis@gmail.com>
2021-04-24 10:17:36 -0400

    Add JWT
    Todo: generate secret

--- backend/routes/reg.js
+++ backend/routes/reg.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 var crypto = require('crypto');
-
+var jwt = require('jsonwebtoken');
 var mongoose = require('mongoose')
+
 const { Schema } = mongoose
 const RegSchema = new Schema({
         height: Number,
@@ -48,7 +49,19 @@ const UserSchema = new Schema({
   salt: String
 });
 
-UserSchema.methods.setPassword = function(password){
+UserSchema.methods.generateJwt = function() {
+    var expiry = new Date();
+    expiry.setDate(expiry.getDate() + 7);
+  
+    return jwt.sign({
+      _id: this._id,
+      email: this.email,
+      name: this.name,
+      exp: parseInt(expiry.getTime() / 1000),
+    }, "MY_SECRET"); // DO NOT KEEP YOUR SECRET IN THE CODE!
+};
+
+  UserSchema.methods.setPassword = function(password){
     this.salt = crypto.randomBytes(16).toString('hex');
     this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
 };
