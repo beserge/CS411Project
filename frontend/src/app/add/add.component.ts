470322fa8ea5306952537f25c650b2b61fe0ba95
beserge <bensergentanis@gmail.com>
2021-04-30 08:22:34 -0400

    fix frontend workout form

--- frontend/src/app/add/add.component.ts
+++ frontend/src/app/add/add.component.ts
@@ -16,42 +16,48 @@ export class AddComponent implements OnInit {
               private auth: AuthenticationService) { }
 
   addForm: FormGroup | any;
-  public workouttypes = ['isOutdoor','isIndoor','isCycling','isRunning']
   ngOnInit() {
     this.addForm = this.formBuilder.group({
       id: [],
       Date: ['',Validators.required],
       Time: ['', Validators.required],
       Duration: ['', Validators.required],
-      Type: ['', Validators.required],
+      io: ['', Validators.required],
+      cr: ['', Validators.required],
     });
 
   }
 
   onSubmit() {
-    const index = this.workouttypes.indexOf(this.addForm.value.Type, 0);
-    if (index > -1) {
-      this.workouttypes.splice(index, 1);
-    }      
     console.log(this.addForm.value)
     var inputstring = ''
     
     var Year = this.addForm.value.Date.substring(0,4)
     var month = this.addForm.value.Date.substring(5,7)
     var Day = this.addForm.value.Date.substring(8)
-    // inputstring+='time='+this.addForm.value.Time+'&day='+Day+'&month='+month+'&year='
-    // +Year+'&'+this.addForm.value.Type+'=true'+
-    // '&'+this.workouttypes[0]+'=false'+'&'+this.workouttypes[1]+'=false'+'&'+this.workouttypes[2]+'=false' +
-    // '&'+this.workouttypes[3]+'=false
-    inputstring += 'time=11:30' +
-                    '&day=1' +
-                    '&month=1' +
-                    '&year=2021' +
-                    '&isCycling=false' +
-                    '&isRunning=true' +
-                    '&isIndoor=false' +
-                    '&isOutdoor=true' +
-                    '&duration=100'
+    var time = this.addForm.value.Time
+    var duration = this.addForm.value.Duration
+    console.log(this.addForm.value)
+
+    inputstring+='time=' + time +
+    '&day=' + Day +
+    '&month=' + month +
+    '&year=' + Year +
+    '&duration=' + duration +
+    '&isIndoor=' + ('isIndoor' == this.addForm.value.io) +
+    '&isOutdoor=' + ('isOutdoor' == this.addForm.value.io) +
+    '&isRunning=' + ('isRunning' == this.addForm.value.cr) +
+    '&isCycling=' + ('isCycling' == this.addForm.value.cr)
+  
+    // inputstring += 'time=11:30' +
+    //                 '&day=1' +
+    //                 '&month=1' +
+    //                 '&year=2021' +
+    //                 '&isCycling=false' +
+    //                 '&isRunning=true' +
+    //                 '&isIndoor=false' +
+    //                 '&isOutdoor=true' +
+    //                 '&duration=100'
 
     console.log('test!!!!!', inputstring)
     this.auth.addworkout(inputstring)
