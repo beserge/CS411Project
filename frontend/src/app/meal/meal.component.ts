1ce5cf45f16f6814cb84b63af3352058128f3540
lwh20 <lwh0519@bu.edu>
2021-05-02 20:19:26 -0400

    meal entering bug fixed

--- frontend/src/app/meal/meal.component.ts
+++ frontend/src/app/meal/meal.component.ts
@@ -10,7 +10,7 @@ import  {Router } from '@angular/router';
   styleUrls: ['./meal.component.css']
 })
 export class MealComponent implements OnInit {
-  
+  public foodlist: Array<string> = [];
   public foodcal:string = ""
   public foodinfo:any={
     foodname:""
@@ -22,12 +22,16 @@ export class MealComponent implements OnInit {
   public name =''
   public po = 0
   public safat = 0
-  public fat_sum = 0
-  public cal_sum = 0
+  public fat = 0
+  public fat_sum=0
+  public cal = 0
+  public cal_sum=0
   public chol = 0
+  public protein=0
   public protein_sum = 0
+  public carbon=0
   public carbon_sum = 0
-
+  public a = 0
   constructor(private http: HttpClient,
               private auth: AuthenticationService,
               private router: Router, ){}
@@ -43,35 +47,43 @@ export class MealComponent implements OnInit {
         var str = "Nutrition Facts:"+'\n';
         for(var i in response){
           for(var j in response[i]){
-              this.cal_sum+=Math.round(response[i][j]['calories'])
-              this.sugar += Math.round(response[i][j]['sugar_g'])
-              this.fiber += Math.round(response[i][j]['fiber_g'])
-              this.size += Math.round(response[i][j]['serving_size_g'])
-              this.sodium += Math.round(response[i][j]['sodium_mg'])
-              this.name += response[i][j]['name']
-              this.po += Math.round(response[i][j]['potassium_mg'])
-              this.safat += Math.round(response[i][j]['fat_saturated_g'])
-              this.chol += Math.round(response[i][j]['cholesterol_mg'])
-              this.carbon_sum+=Math.round(response[i][j]['carbohydrates_total_g'])
-              this.fat_sum+=Math.round(response[i][j]['fat_total_g'])
-              this.protein_sum+=Math.round(response[i][j]['protein_g'])
+              this.cal=Math.round(response[i][j]['calories'])
+              this.cal_sum+=this.cal;
+              this.sugar = Math.round(response[i][j]['sugar_g'])
+              this.fiber = Math.round(response[i][j]['fiber_g'])
+              this.size = Math.round(response[i][j]['serving_size_g'])
+              this.sodium = Math.round(response[i][j]['sodium_mg'])
+              this.name = response[i][j]['name']
+              this.po = Math.round(response[i][j]['potassium_mg'])
+              this.safat = Math.round(response[i][j]['fat_saturated_g'])
+              this.chol = Math.round(response[i][j]['cholesterol_mg'])
+              this.carbon=Math.round(response[i][j]['carbohydrates_total_g'])
+              this.carbon_sum+=this.carbon;
+              this.fat=Math.round(response[i][j]['fat_total_g'])
+              this.fat_sum+=this.fat;
+              this.protein=Math.round(response[i][j]['protein_g'])
+              this.protein_sum+=this.protein;
           }
         } str+="Total Calories="+this.cal_sum+'\n'+"Total Carbonhydrates="+this.carbon_sum+'\n'+
             "Total fat="+this.fat_sum+'\n'+ "Total Protein="+this.protein_sum;
         this.foodcal = str
-      })
+      });
+      this.foodlist.push(JSON.stringify(this.foodinfo.foodname));
+      this.a=1;
   }
   Submit_back():void{
     var backendstr:string = 'sugar_g='+this.sugar+'&fiber_g='+this.fiber+'&serving_size_g='+
       this.size+'&sodium_mg='+this.sodium+'&name='+this.name+'&potassium_mg='+this.po+'&fat_saturated_g='
-      +this.safat+'&fat_total_g='+this.fat_sum+'&calories='+this.cal_sum+'&cholesterol_mg='+this.chol
-      +'&protein_g='+this.carbon_sum+'&carbohydrates_total_g='+this.protein_sum
+      +this.safat+'&fat_total_g='+this.fat+'&calories='+this.cal+'&cholesterol_mg='+this.chol
+      +'&protein_g='+this.carbon+'&carbohydrates_total_g='+this.protein
     this.auth.meal_post(backendstr).subscribe((response)=>
       {
       if(response.message=="Meal added to DB"){alert("Successfully Added")}
       else{
       alert("Failed to Add")}
     })
+    this.foodinfo.foodname=""
+    this.a=0;
   }
 
   
